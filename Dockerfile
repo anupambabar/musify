198136ed040d7f674a4ec22a9f5f7f6e0017e0f0
Anupam Babar <anupam.babar12@gmail.com>
2023-07-16 23:20:00 +0530

    Added Docker Compose File to bind all docker images

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM openjdk:17
-VOLUME /tmp
+VOLUME /musify
 EXPOSE 8081
 ARG JAR_FILE=target/musify-0.0.1-SNAPSHOT.jar
 ADD ${JAR_FILE} musify-0.0.1-SNAPSHOT.jar
--- src/test/java/com/musify/controller/ArtistControllerIntegrationTest.java
+++ src/test/java/com/musify/controller/ArtistControllerIntegrationTest.java
@@ -6,16 +6,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ArtistControllerIntegrationTest {
 
+    @Container
+    static GenericContainer redis = new GenericContainer(DockerImageName.parse("redis"))
+            .withExposedPorts(6379);
     @Autowired
     private TestRestTemplate restTemplate;
     @LocalServerPort
     private int randomServerPort;
+
+    @DynamicPropertySource
+    static void redisProperties(DynamicPropertyRegistry registry) {
+        redis.start();
+        registry.add("spring.redis.host", redis::getHost);
+        registry.add("spring.redis.port", redis::getFirstMappedPort);
+    }
 
     @Test
     void getArtist() {
